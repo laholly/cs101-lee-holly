5d95b8b4d9d4021d990cc9768d2b951a686b4ccd
Lee Holly <lholly@gmail.com>
2025-07-31 12:53:36 -0400

    Update to main. Initial commit of inputData

--- cs101-lee-holly/main.cpp
+++ cs101-lee-holly/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
+string myString;
+int x_value;
+string char_value;
+int y_value;
 
 int main() {
-    cout << "Hello from clean slate!" << endl;
-    return 0;
+    ifstream inputFile("/Users/leeholly/CS101/cs101-lee-holly/cs101-lee-holly/inputData.txt");
+    if (!inputFile.is_open()) {
+        cerr << "Failed to open file for reading.\n";
+    }
+    while (getline(inputFile, myString)) {
+        stringstream inputLine(myString);
+        inputLine >> x_value;
+        inputLine >> char_value;
+        inputLine >> y_value;
+        cout << x_value << " " << char_value << " " << y_value << endl;
+        
+        
+    }
 }
 
