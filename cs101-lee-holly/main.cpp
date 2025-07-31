661dd051c5ac3f4c81d399ed055e3eefc4c0ea02
Lee Holly <lholly@gmail.com>
2025-07-30 22:37:54 -0400

    Initial Commit

---
+++ cs101-lee-holly/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cs101-lee-holly
+//
+//  Created by Lee Holly on 7/30/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
